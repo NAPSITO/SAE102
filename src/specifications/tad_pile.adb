efa7a79cdf294cc71f985e7a7a7d6d1e8c124f95
Ros Natalia <natalia.ros@etu.iut-tlse3.fr>
2024-01-22 08:54:01 +0100

    add tad_pile.adb

---
+++ src/specifications/tad_pile.adb
@@ -0,0 +1,55 @@
+pragma Ada_2012;
+package body TAD_Pile is
+
+   --------------------
+   -- construirePile --
+   --------------------
+
+   function construirePile return Type_Pile is
+   begin
+      pragma Compile_Time_Warning
+        (Standard.True, "construirePile unimplemented");
+      return raise Program_Error with "Unimplemented function construirePile";
+   end construirePile;
+
+   -------------
+   -- estVide --
+   -------------
+
+   function estVide (pile : in Type_Pile) return Boolean is
+   begin
+      pragma Compile_Time_Warning (Standard.True, "estVide unimplemented");
+      return raise Program_Error with "Unimplemented function estVide";
+   end estVide;
+
+   -------------
+   -- dernier --
+   -------------
+
+   function dernier (pile : in Type_Pile) return T is
+   begin
+      pragma Compile_Time_Warning (Standard.True, "dernier unimplemented");
+      return raise Program_Error with "Unimplemented function dernier";
+   end dernier;
+
+   -------------
+   -- empiler --
+   -------------
+
+   function empiler (pile : in Type_Pile; e : in T) return Type_Pile is
+   begin
+      pragma Compile_Time_Warning (Standard.True, "empiler unimplemented");
+      return raise Program_Error with "Unimplemented function empiler";
+   end empiler;
+
+   -------------
+   -- depiler --
+   -------------
+
+   function depiler (pile : in Type_Pile) return Type_Pile is
+   begin
+      pragma Compile_Time_Warning (Standard.True, "depiler unimplemented");
+      return raise Program_Error with "Unimplemented function depiler";
+   end depiler;
+
+end TAD_Pile;
